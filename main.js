eb7ccd818d66a8f40458741d3f0a28acad1705d4
Meryembentalebeljadil <meryembentaleb11@gmail.com>
2021-04-07 10:26:08 +0200

    js-exo2-part3

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('show').addEventListener('click', function(){
+    var action1 = document.getElementById('text').style.display='block';   
+})
+document.getElementById('hide').addEventListener('click', function(){
+    var action2 = document.getElementById('text').style.display='none';   
+})
